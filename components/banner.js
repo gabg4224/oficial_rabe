e268957f046d6b80c0319fd92429b531a35a6cca
gabg4224 <86382892+gabg4224@users.noreply.github.com>
2023-10-20 15:11:22 -0300

    [refactor] banner & cardHome
    refactor  to have a better perfomance across media querys

--- components/banner.js
+++ components/banner.js
@@ -1,18 +1,41 @@
 import CardHome from "./cardHome";
 
-
 export default function Banner() {
+const endpoints = [
+  "https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/portada/portada-web.webp?t=2023-10-20T17%3A56%3A05.311Z",
+  "https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/portada/empaques-piso-2.webp",
+  "https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/portada/cielo-producto-dragonball.webp",
+  "https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/portada/suetere-portada.webp?t=2023-10-20T17%3A57%3A18.760Z",
+  "https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/portada/televisor-con-todas-la-franelas.webp?t=2023-10-20T17%3A57%3A26.989Z"
+]
   return (
-    <section className=" bg-[#f5f5f5]">
-      <div className=" flex w-full  justify-center items-center sm:py-6 md:py-10 lg:py-20 sm:px-6 lg:px-14">
-        <div className="grid w-full sm:grid-cols-1  sm:grid-rows-5 md:grid-cols-2 md:grid-rows-4  lg:grid-cols-4 lg:grid-rows-2  grid-cols-2 min-h-[66vh]  grid-rows-4  gap-4">
-          <CardHome view={"major"}></CardHome>
-          <CardHome></CardHome>
-          <CardHome></CardHome>
-          <CardHome></CardHome>
-          <CardHome></CardHome>
+    <>
+  <section className=" bg-[#f5f5f5] ">
+        <div className=" w-full lg:h-[750px] h-full sm:py-6 md:py-10 lg:py-12 sm:px-6 lg:px-14 ">
+          <div className="grid h-full mx-auto  w-full max-w-[1200px] gap-4 sm:grid-cols-1  sm:grid-rows-5 md:grid-cols-2 md:grid-rows-4  lg:grid-cols-4 lg:grid-rows-2 ">
+            {endpoints?.map((item, index) => (
+              <CardHome
+                key={crypto.randomUUID}
+                src={item}
+                index={index}
+              ></CardHome>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+
+     {/*  <section className=" bg-[#f5f5f5]">
+        <div className=" flex w-full  justify-center items-center sm:py-6 md:py-10 lg:py-20 sm:px-6 lg:px-14">
+          <div className="grid w-full max-w-5xlxl sm:grid-cols-1  sm:grid-rows-5 md:grid-cols-2 md:grid-rows-4  lg:grid-cols-4 lg:grid-rows-2  grid-cols-2 min-h-[66vh]  grid-rows-4  gap-4">
+            <CardHome view={"major"}></CardHome>
+            <CardHome></CardHome>
+            <CardHome></CardHome>
+            <CardHome></CardHome>
+            <CardHome></CardHome>
+          </div>
+        </div>
+      </section> */}
+    </>
   );
 }
+
--- components/cardHome.js
+++ components/cardHome.js
@@ -1,60 +1,33 @@
 import Image from "next/image";
 
-export default function CardHome({ view }) {
-  if (view === "major") {
-    return (
-      <div
-        className={`md:row-span-2 md:col-span-2 sm:row-span-1 sm:col-span-1 relative group flex justify-center items-end h-full overflow-hidden  `}
-      >
-        <Image
-          src={"https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/image1.png"}
-          width={1280}
-          height={1707}
-          className="h-full group-hover:scale-110 transition-all aspect-auto duration-700 ease-out  opacity-0 "
-          onLoadingComplete={(image)=> image.classList.remove("opacity-0")}
-          alt="image"
-        ></Image>
-        <div className="absolute inset-0 z-10 flex justify-center items-end p-10 px-3 text-center">
-          <div className="flex flex-col gap-3">
-            <div>
-              <h4 className="font-bold">new year</h4>
-              <p className="font-light">new design</p>
-            </div>
-            <button className="capitalize bg-black rounded-lg text-white text-sm p-2 px-5 ">
-              shop latest
-            </button>
-          </div>
+export default function CardHome({ index,src }) {
+  return (
+    <div
+      className={`${
+        index == 0
+          ? "md:row-span-2 md:col-span-2 sm:row-span-1 sm:col-span-1 relative group flex justify-center items-end h-full overflow-hidden  "
+          : "sm:row-span-1 sm:col-span-1 col-span-1 relative group flex justify-center items-end h-full overflow-hidden "
+      }  rounded-md`}
+    >
+      <Image
+        src={
+          src
+        }
+        width={1280}
+        height={1707}
+        className="h-full group-hover:scale-110 transition-all aspect-auto duration-700 ease-out  opacity-0 "
+        onLoadingComplete={(image) => image.classList.remove("opacity-0")}
+        alt="image"
+      ></Image>
+      <div className="absolute inset-0 z-10 flex justify-center items-end p-10 px-3 text-center">
+        <div className="flex flex-col gap-3">
+        
+          <button className="uppercase bg-[#ff2040] rounded-lg text-white text-sm p-2 px-5 ">
+          Comprar Ahora
+          </button>
         </div>
       </div>
-    );
-  }
-  if (view !== "major") {
-    return (
-      <div
-        className={`sm:row-span-1 sm:col-span-1 col-span-1 relative group flex justify-center items-end h-full overflow-hidden  `}
-      >
-        <Image
-          src={"https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/image1.png"}
-          width={1280}
-          height={1707}
-          className="h-full group-hover:scale-110  aspect-auto transition-all duration-700 ease-out opacity-0 "
-          placeholder="blur"
-          blurDataURL="https://zwinwrcelthkeoztdgcd.supabase.co/storage/v1/object/public/rabe-images/rabe-16x16.svg"
-          alt="image"
-          onLoadingComplete={(image)=> image.classList.remove("opacity-0")}
-        ></Image>
-        <div className="absolute inset-0 z-10 flex justify-center items-end p-10 px-3 text-center">
-          <div className="flex flex-col gap-3">
-            <div>
-              <h4 className="font-bold">new year</h4>
-              <p className="font-light">new design</p>
-            </div>
-            <button className="capitalize bg-black rounded-lg text-white text-sm p-2 px-5 ">
-              shop latest
-            </button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
+
 }
